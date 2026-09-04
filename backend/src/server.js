33dd1f120cca949603cb90f022f1229afc2e1194
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-04 14:01:17 +0700

    feat: implement CRUD routes for users and add REST testing file in backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,75 +8,97 @@ app.use(express.json());
 // CRUD routes and endpoints
 
 // Read users
-app.get("/users", (req, res) => {
-  res.send(users);
+app.get("/users", (req, res, next) => {
+  try {
+    res.send(users);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // Create users
-app.post("/users", (req, res) => {
-  const { username, email, password } = req.body;
+app.post("/users", (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
 
-  if (!username || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: "username, email and password are required!" });
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "username, email and password are required!" });
+    }
+
+    const hightestId = users.reduce(
+      (max, user) => Math.max(max, Number(user.id)),
+      0,
+    );
+
+    const nextId = String(hightestId + 1);
+
+    const newUser = {
+      id: nextId,
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    users.push(newUser);
+    return res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
   }
-
-  const hightestId = users.reduce(
-    (max, user) => Math.max(max, Number(user.id)),
-    0,
-  );
-
-  const nextId = String(hightestId + 1);
-
-  const newUser = {
-    id: nextId,
-    username: username,
-    email: email,
-    password: password,
-  };
-
-  users.push(newUser);
-
-  return res.status(201).json(newUser);
 });
 
 // Update users
-app.put("/users/:id", (req, res) => {
-  const user = users.find((u) => u.id === req.params.id);
+app.put("/users/:id", (req, res, next) => {
+  try {
+    const user = users.find((u) => u.id === req.params.id);
 
-  if (!user) {
-    return res.status(404).json({ error: "User not found!" });
+    if (!user) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "username, email and password are required!" });
+    }
+
+    user.username = username;
+    user.email = email;
+    user.password = password;
+
+    return res.status(200).json(user);
+  } catch (err) {
+    next(err);
   }
-
-  const { username, email, password } = req.body;
-
-  if (!username || !email || !password) {
-    return res
-      .status(400)
-      .json({ error: "username, email and password are required!" });
-  }
-
-  user.username = username;
-  user.email = email;
-  user.password = password;
-
-  return res.status(200).json(user);
 });
 
 // Delete users
-app.delete("/users/:id", (req, res) => {
-  const user = users.find((u) => u.id === req.params.id);
+app.delete("/users/:id", (req, res, next) => {
+  try {
+    // const user = users.find((u) => u.id === req.params.id);
 
-  if (!user) {
-    return res.status(404).json({ error: "User not found!" });
+    // if (!user) {
+    //   return res.status(404).json({ error: "User not found!" });
+    // }
+
+    // const index = users.indexOf(user);
+    // users.splice(index, 1);
+
+    const index = users.findIndex((u) => u.id === req.params.id);
+
+    if (index === -1) {
+      return res.status(404).json({ error: "User not found!" });
+    }
+
+    users.splice(index, 1);
+
+    return res.status(204).send();
+  } catch (err) {
+    next(err);
   }
-
-  const index = users.indexOf(user);
-
-  users.splice(index, 1);
-
-  return res.status(204).send();
 });
 
 // Centralize Error Handling Middleware
