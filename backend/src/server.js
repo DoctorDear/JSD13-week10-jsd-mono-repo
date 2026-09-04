8aa7340791eb243b636700ab57b0901a0e274357
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-04 15:45:38 +0700

    feat: implement Express server and modularized user CRUD API routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,105 +1,13 @@
 import express from "express";
 import { users } from "./fakeDB/fakeUsers.js";
-
+import { routes as apiRoutes } from "./routes/index.js";
 const app = express();
 
 app.use(express.json());
 
 // CRUD routes and endpoints
 
-// Read users
-app.get("/users", (req, res, next) => {
-  try {
-    res.send(users);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Create users
-app.post("/users", (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
-
-    if (!username || !email || !password) {
-      return res
-        .status(400)
-        .json({ error: "username, email and password are required!" });
-    }
-
-    const hightestId = users.reduce(
-      (max, user) => Math.max(max, Number(user.id)),
-      0,
-    );
-
-    const nextId = String(hightestId + 1);
-
-    const newUser = {
-      id: nextId,
-      username: username,
-      email: email,
-      password: password,
-    };
-
-    users.push(newUser);
-    return res.status(201).json(newUser);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Update users
-app.put("/users/:id", (req, res, next) => {
-  try {
-    const user = users.find((u) => u.id === req.params.id);
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found!" });
-    }
-
-    const { username, email, password } = req.body;
-
-    if (!username || !email || !password) {
-      return res
-        .status(400)
-        .json({ error: "username, email and password are required!" });
-    }
-
-    user.username = username;
-    user.email = email;
-    user.password = password;
-
-    return res.status(200).json(user);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Delete users
-app.delete("/users/:id", (req, res, next) => {
-  try {
-    const user = users.find((u) => u.id === req.params.id);
-
-    if (!user) {
-      return res.status(404).json({ error: "User not found!" });
-    }
-
-    const index = users.indexOf(user);
-    users.splice(index, 1);
-
-    // const index = users.findIndex((u) => u.id === req.params.id);
-
-    // if (index === -1) {
-    //   return res.status(404).json({ error: "User not found!" });
-    // }
-
-    // users.splice(index, 1);
-
-    return res.status(204).send();
-  } catch (err) {
-    next(err);
-  }
-});
+app.use("/api", apiRoutes);
 
 // Centralize Error Handling Middleware
 app.use((err, req, res, next) => {
