3d6ef7ffba2c7e6a312384cca317b3561c66bf5b
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-07 14:11:28 +0700

    feat: add User mongoose model and REST API test suite for user operations

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-    username: { type: String },
-    email: { type: String },
+    username: { type: String, unique: true },
+    email: { type: String, unique: true },
     password: { type: String, select: false },
   },
   { timestamps: true },
