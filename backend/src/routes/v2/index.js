04a7b83b43cbf674054a95c3a95db902168743c0
Chirasak Tapphae <dearchirasak@gmail.com>
2026-09-08 13:31:24 +0700

    feat: implement v2 Supabase user routes and add corresponding REST test file

--- backend/src/routes/v2/index.js
+++ backend/src/routes/v2/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { router as userRoutes } from "./user.routes.js";
+import { router as userSupabaseRoutes } from "./user.supabase.routes.js";
 
 export const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/users", userSupabaseRoutes);
